ce21b05bf8f448937ae31ef3987907b1264e2564
lkmlkm073 <sju4867@gmail.com>
2019-06-29 23:37:11 -0700

    Added login page, image issues still there, and added new gemfile

--- SFU_Link/config/routes.rb
+++ SFU_Link/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+  get 'pages/about', to: 'pages#about'
+  get 'pages/contact', to: 'pages#contact'
+  get 'pages/login', to: 'pages#login'
 end
