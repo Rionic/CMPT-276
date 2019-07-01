e250779557ed67840ec132859d7bb5b32207f3ee
lkmlkm073 <sju4867@gmail.com>
2019-06-30 22:55:08 -0700

    carousel images are now fixed, model and db for User is added with new signup form

--- SFU_Link/config/routes.rb
+++ SFU_Link/config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'pages/about', to: 'pages#about'
   get 'pages/contact', to: 'pages#contact'
-  get 'pages/login', to: 'pages#login'
   get 'pages/signup', to: 'pages#signup'
 end
