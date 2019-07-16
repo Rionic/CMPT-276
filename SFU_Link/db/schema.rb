3769a68fc8404a94ecd7ae381b4a58e8bb46b92a
lkmlkm073 <sju4867@gmail.com>
2019-07-15 19:51:10 -0700

    Add password confirmation, changed profile page and User schema

--- SFU_Link/db/schema.rb
+++ SFU_Link/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_01_061144) do
+ActiveRecord::Schema.define(version: 2019_07_16_014425) do
 
   create_table "users", force: :cascade do |t|
     t.string "firstname"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_07_01_061144) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.text "description"
+    t.integer "faculty"
   end
 
 end
